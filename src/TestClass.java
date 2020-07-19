6aaa78ba9ffd5daa02ab8fc374434c8371145ba2
ilhan44-44 <68486889+ilhan44-44@users.noreply.github.com>
2020-07-19 23:19:46 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,7 +1,7 @@
 public class TestClass {
     public static void main(String[] args) {
      System.out.println("Git'den Merhaba");
-      system.out.  println("GitHubrm);
+      system.out.  println("GitHub'dan Merhaba);
         
         
     }
